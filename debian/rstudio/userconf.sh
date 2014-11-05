916b7b3998e0669c8b301370d169f9197a6bdb46
Carl Boettiger <cboettig@gmail.com>
2014-11-05 11:25:45 -0800

    update userconf to avoid && after mkdir if mkdir fails, e.g. because the directory already exists as a linked volume, we still want to make sure that it's owned by the correct user

--- debian/rstudio/userconf.sh
+++ debian/rstudio/userconf.sh
@@ -9,10 +9,13 @@ USERID=${USERID:=1000}
 ## Things get messy if we have more than one user.  Best to delete it.  
 userdel docker
 ## Configure user account name and password (used by rstudio)
-useradd -m $USER -u $USERID && echo "$USER:$PASSWORD" | chpasswd
+useradd -m $USER -u $USERID 
+echo "$USER:$PASSWORD" | chpasswd
 ## User must own their home directory, or RStudio won't be able to load
 ## (Note this is only necessary if the user is linking a shared volume to a subdir of this directory)
-mkdir /home/$USER && chown $USER:$USER /home/$USER
+mkdir /home/$USER 
+## Must be on a separate line, since we want this to run even if /home/$USER already exists and thus mkdir fails
+chown $USER:$USER /home/$USER
 
 
 ## Configure git user to avoid being harassed about this later
