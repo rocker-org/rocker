aaeece9fb01bd440c5b2756fbd9f7857d456ab1a
Carl Boettiger <cboettig@gmail.com>
2015-01-04 12:17:36 -0800

    tidy up comments, user configuration

--- rstudio/userconf.sh
+++ rstudio/userconf.sh
@@ -8,6 +8,7 @@ USERID=${USERID:=1000}
 ROOT=${ROOT:=FALSE}
 
 ## Things get messy if we have more than one user.  Best to delete it.  
+## (Docker cares only about uid, not username; diff users with same uid = confusion)
 userdel docker
 ## Configure user account name and password (used by rstudio)
 useradd -m $USER -u $USERID 
@@ -15,13 +16,8 @@ echo "$USER:$PASSWORD" | chpasswd
 ## User must own their home directory, or RStudio won't be able to load
 ## (Note this is only necessary if the user is linking a shared volume to a subdir of this directory)
 mkdir /home/$USER 
-## Configure git 
+## Configure git for the User. Since root is running this script, cannot use `git config`
 echo -e "[user]\n\tname = $USER\n\temail = $EMAIL\n\n[credential]\n\thelper = cache\n\n[push]\n\tdefault = simple\n\n[core]\n\teditor = vim\n" > /home/$USER/.gitconfig
-
-chown $USER:$USER /home/$USER
-
-## Global config make page-up/down search history
-echo '"\e[5~": history-search-backward' >> /etc/inputrc && echo '"\e[6~": history-search-backward' >> /etc/inputrc
 
 ## Let user write to /usr/local/lib/R/site.library
 addgroup $USER staff
@@ -34,3 +30,5 @@ fi
 ## Symlink pandoc templates to default directory
 mkdir /home/$USER/.pandoc && ln -s /opt/pandoc/templates /home/$USER/.pandoc/templates
 
+## User should own their own home directory and all containing files
+chown $USER:$USER /home/$USER
