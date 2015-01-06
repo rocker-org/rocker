7c910e3f8757adbacb19f27eecde33cffd90516b
Carl Boettiger <cboettig@gmail.com>
2015-01-05 16:39:06 -0800

    handle some edge cases regarding user and sudo

--- rstudio/userconf.sh
+++ rstudio/userconf.sh
@@ -6,6 +6,10 @@ if [ "$EUID" -ne 0 ]
 	exit
 fi
 
+## If we run as sudo, $USER is assigned to 'root' instead of undefined, and `sudo supervisord` doesn't set the default Ideally we would be able to get the actual username when sudo is used, but this is tricky.  Anyway, we never intend to log into RStudio directly as 'root' since it won't work anyway.
+if [ "$USER" = root ]
+	unset USER
+
 ## Set defaults for environmental variables in case they are undefined
 USER=${USER:=rstudio}
 PASSWORD=${PASSWORD:=rstudio}
